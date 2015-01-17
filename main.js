dbce58603bf270d7212700b5775b3f7cad840c99
Dustin Morando <dustinmorando@me.com>
2015-01-17 12:27:02 -0500

    removed all console logs

--- main.js
+++ main.js
@@ -3,35 +3,73 @@
 
   var operation;
 
-  
+  // //this is the pressed number
+  function ninePressed(event) {
+    event.preventDefault();
+    numberPressed(Number(9));
+  }
+  function eightPressed(event) {
+    event.preventDefault();
+    numberPressed(Number(8));
+  }
+  function sevenPressed(event) {
+    event.preventDefault();
+    numberPressed(Number(7));
+  }
+  function sixPressed(event) {
+    event.preventDefault();
+    numberPressed(Number(6));
+  }
+  function fivePressed(event) {
+    event.preventDefault();
+    numberPressed(Number(5));
+  }
+  function fourPressed(event) {
+    event.preventDefault();
+    numberPressed(Number(4));
+  }
+  function threePressed(event) {
+    event.preventDefault();
+    numberPressed(Number(3));
+  }
+  function twoPressed(event) {
+    event.preventDefault();
+    numberPressed(Number(2));
+  }
+  function onePressed(event) {
+    event.preventDefault();
+    numberPressed(Number(1));
+  }
+  function zeroPressed(event) {
+    event.preventDefault();
+    numberPressed(Number(0));
+  }
+
   function plusPressed(event){
     event.preventDefault();
     operation = '+';
-    console.log('yes');
+
   }
   function minusPressed(event){
     event.preventDefault();
     operation = '-';
-    console.log('pig');
   }
   function mutiplyPressed(event){
     event.preventDefault();
     operation = '*';
-    console.log('pig');
   }
   function dividePressed(event){
     event.preventDefault();
     operation = '/';
-    console.log('pig');
   }
 
 
   function numberPressed(number){
-    console.log('clown');
+
 
     // Get the displayed number
     var displayedNumber = Number(document.getElementById('display').value);
-    console.log('no');
+
 
     // Calculate the new number
     if(operation == '+') {
@@ -44,81 +82,16 @@
       displayedNumber = displayedNumber / number;
     } else {
       displayedNumber = number;
-      console.log('maybe');
     }
 
     // Display the new number
     document.getElementById('display').value = displayedNumber;
-    console.log('fox');
   }
-
-// //this is the pressed number
-  function ninePressed(event) {
-    event.preventDefault();
-    numberPressed(Number(9));
-    console.log('hound');
-  }
-  function eightPressed(event) {
-    event.preventDefault();
-    numberPressed(Number(8));
-    console.log('round');
-  }
-  function sevenPressed(event) {
-    event.preventDefault();
-    numberPressed(Number(7));
-    console.log('round');
-  }
-  function sixPressed(event) {
-    event.preventDefault();
-    numberPressed(Number(6));
-    console.log('round');
-  }
-  function fivePressed(event) {
-    event.preventDefault();
-    numberPressed(Number(5));
-    console.log('round');
-  }
-  function fourPressed(event) {
-    event.preventDefault();
-    numberPressed(Number(4));
-    console.log('round');
-  }
-  function threePressed(event) {
-    event.preventDefault();
-    numberPressed(Number(3));
-    console.log('round');
-  }
-  function twoPressed(event) {
-    event.preventDefault();
-    numberPressed(Number(2));
-    console.log('round');
-  }
-  function onePressed(event) {
-    event.preventDefault();
-    numberPressed(Number(1));
-    console.log('round');
-  }
-  function zeroPressed(event) {
-    event.preventDefault();
-    numberPressed(Number(0));
-    console.log('round');
-  }
-
-
-
-
-
-
-
-
-
-
 
 
 // telling the computer to read the HTML and listen for a click
 
   document.getElementById('9').addEventListener('click', ninePressed);
-  console.log('baby');
   document.getElementById('8').addEventListener('click', eightPressed);
   document.getElementById('7').addEventListener('click', sevenPressed);
   document.getElementById('6').addEventListener('click', sixPressed);
